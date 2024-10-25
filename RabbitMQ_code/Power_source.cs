ec120cade0c11fe78ec0f988384d43a1bc6b476f
fried <friedemann1999@googlemail.com>
2024-10-25 10:05:43 +0200

    Added code for the simulation and meters

---
+++ RabbitMQ_code/Power_source.cs
@@ -0,0 +1,11 @@
+namespace DefaultNamespace;
+
+public class Power_source
+// the parent class that power sources inherit from
+{
+	private int id;
+	private simhost = SimulationHost.getinstance();
+	public virtual float get_power(){
+		
+	}
+}
