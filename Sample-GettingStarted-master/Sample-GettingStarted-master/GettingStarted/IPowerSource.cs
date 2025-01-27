81a920a63bd60170327ad35f760ea384227a9fd0
fridi99 <142048507+fridi99@users.noreply.github.com>
2025-01-27 17:34:09 +0100

    chekcup

--- Sample-GettingStarted-master/Sample-GettingStarted-master/GettingStarted/IPowerSource.cs
+++ Sample-GettingStarted-master/Sample-GettingStarted-master/GettingStarted/IPowerSource.cs
@@ -2,6 +2,7 @@ using System;
 namespace DefaultNamespace;
 
 public interface IPowerSource
+// Interface for all the Power Classes. Only enforces the single interface function
 {
     public abstract float get_power(DateTime time, float weatherfactor = 100);
 }
